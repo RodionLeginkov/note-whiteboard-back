a1a66c35b118cc22d3033f2d2330f1cfe511e691
RodionLeginkov <rodion.leg.work@gmail.com>
2021-03-11 11:14:00 +0300

    Export add try catch

--- router/export/export.js
+++ router/export/export.js
@@ -12,57 +12,65 @@ router.get('/fetch-pdf', async (req, res) => {
 });
 
 router.get('/generateReport', aunteficate(), async (req, res) => {
-  if (fs.access(`${__dirname}/report.pdf`)) {
-    await fs.unlink(`${__dirname}/report.pdf`);
-  }
-  const userId = res.locals.user.id;
-  const categories = await Category.findAll({
-    order: [['title']],
-    include: [{
-      model: Note,
-      as: 'taskIds',
-      required: false,
-      attributes: ['id'],
-    }],
-    where: { UserId: userId },
-    raw: true,
-  });
-  if (!categories) {
-    return res.status(403).json({ message: 'something went wrong' });
-  }
-  const mapAsync = (arr, func) => Promise.all(arr.map(func));
-  await mapAsync(categories, async (category) => {
-    if (!category['taskIds.id']) {
-      category.svg = '';
-      return null;
+  try {
+    fs.access(`${__dirname}/report.pdf`, fs.F_OK, async (err) => {
+      if (err) {
+        console.error(err);
+      }
+      await fs.unlink(`${__dirname}/report.pdf`);
+    });
+    const userId = res.locals.user.id;
+    const categories = await Category.findAll({
+      order: [['title']],
+      include: [{
+        model: Note,
+        as: 'taskIds',
+        required: false,
+        attributes: ['id'],
+      }],
+      where: { UserId: userId },
+      raw: true,
+    });
+    if (!categories) {
+      return res.status(403).json({ message: 'something went wrong' });
     }
-    const id = category['taskIds.id'];
-    category.svg = `${await fs.readFile(`${__dirname}/../note/svg/${id}`)}`;
-  });
+    const mapAsync = (arr, func) => Promise.all(arr.map(func));
+    await mapAsync(categories, async (category) => {
+      if (!category['taskIds.id']) {
+        category.svg = '';
+        return null;
+      }
+      const id = category['taskIds.id'];
+      category.svg = `${await fs.readFile(`${__dirname}/../note/svg/${id}`)}`;
+    });
 
-  ejs.renderFile(path.join(__dirname, './views/template.ejs'), { categories }, (err, data) => {
-    if (err) {
-      res.send(err);
-    } else {
-      const options = {
-        height: '8.25in',
-        width: '8.5in',
-        header: {
-          height: '20mm',
-        },
-        footer: {
-          height: '20mm',
-        },
-      };
-      pdf.create(data, options).toFile(`${__dirname}/report.pdf`, (err, data) => {
-        if (err) {
-          res.send(err);
-        } else {
-          res.send('File created successfully');
-        }
-      });
-    }
-  });
+    ejs.renderFile(path.join(__dirname, './views/template.ejs'), { categories }, (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        const options = {
+          height: '8.25in',
+          width: '8.5in',
+          header: {
+            height: '20mm',
+          },
+          footer: {
+            height: '20mm',
+          },
+        };
+        pdf.create(data, options).toFile(`${__dirname}/report.pdf`, (err, data) => {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send('File created successfully');
+          }
+        });
+      }
+    });
+  } catch (err) {
+    console.log('err', err);
+    return res.status(403).json({ message: err });
+  }
 });
 
 module.exports = router;
